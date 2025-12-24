a5ea1a4d7c1d82ed4f203b9cf07436640ee921d8
Benjamin Liu <benjaminliu_dev@outlook.com>
2025-12-23 19:26:52 -0800

    Revert "fix profiling and format code."
    This reverts commit 9767506616e2423a3f7663072330f5e595784424.
    
    	modified:   .vscode/c_cpp_properties.json
    	modified:   .vscode/settings.json
        modified:   src/autons.cpp
    	modified:   src/main.cpp
    	modified:   src/robot-config.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -8,8 +8,7 @@
  * exit conditions, check the docs.
  */
 
-void default_constants()
-{
+void default_constants(){
   // Each constant set is in the form of (maxVoltage, kP, kI, kD, startI).
   chassis.set_drive_constants(10, 1.5, 0, 10, 0);
   chassis.set_heading_constants(6, .4, 0, 1, 0);
@@ -28,8 +27,7 @@ void default_constants()
  * a slower max_voltage and greater settle_error than you would otherwise.
  */
 
-void odom_constants()
-{
+void odom_constants(){
   default_constants();
   chassis.heading_max_voltage = 10;
   chassis.drive_max_voltage = 8;
@@ -42,8 +40,7 @@ void odom_constants()
  * The expected behavior is to return to the start position.
  */
 
-void drive_test()
-{
+void drive_test(){
   chassis.drive_distance(6);
   chassis.drive_distance(12);
   chassis.drive_distance(18);
@@ -54,8 +51,7 @@ void drive_test()
  * The expected behavior is to return to the start angle, after making a complete turn.
  */
 
-void turn_test()
-{
+void turn_test(){
   chassis.turn_to_angle(5);
   chassis.turn_to_angle(30);
   chassis.turn_to_angle(90);
@@ -67,8 +63,7 @@ void turn_test()
  * Should swing in a fun S shape.
  */
 
-void swing_test()
-{
+void swing_test(){
   chassis.left_swing_to_angle(90);
   chassis.right_swing_to_angle(0);
 }
@@ -77,8 +72,7 @@ void swing_test()
  * A little of this, a little of that; it should end roughly where it started.
  */
 
-void full_test()
-{
+void full_test(){
   chassis.drive_distance(24);
   chassis.turn_to_angle(-45);
   chassis.drive_distance(-36);
@@ -88,22 +82,20 @@ void full_test()
 }
 
 /**
- * Doesn't drive the robot, but just prints coordinates to the Brain screen
+ * Doesn't drive the robot, but just prints coordinates to the Brain screen 
  * so you can check if they are accurate to life. Push the robot around and
  * see if the coordinates increase like you'd expect.
  */
 
-void odom_test()
-{
+void odom_test(){
   chassis.set_coordinates(0, 0, 0);
-  while (1)
-  {
+  while(1){
     Brain.Screen.clearScreen();
-    Brain.Screen.printAt(5, 20, "X: %f", chassis.get_X_position());
-    Brain.Screen.printAt(5, 40, "Y: %f", chassis.get_Y_position());
-    Brain.Screen.printAt(5, 60, "Heading: %f", chassis.get_absolute_heading());
-    Brain.Screen.printAt(5, 80, "ForwardTracker: %f", chassis.get_ForwardTracker_position());
-    Brain.Screen.printAt(5, 100, "SidewaysTracker: %f", chassis.get_SidewaysTracker_position());
+    Brain.Screen.printAt(5,20, "X: %f", chassis.get_X_position());
+    Brain.Screen.printAt(5,40, "Y: %f", chassis.get_Y_position());
+    Brain.Screen.printAt(5,60, "Heading: %f", chassis.get_absolute_heading());
+    Brain.Screen.printAt(5,80, "ForwardTracker: %f", chassis.get_ForwardTracker_position());
+    Brain.Screen.printAt(5,100, "SidewaysTracker: %f", chassis.get_SidewaysTracker_position());
     task::sleep(20);
   }
 }
@@ -113,13 +105,12 @@ void odom_test()
  * will be curved while the first is straight.
  */
 
-void tank_odom_test()
-{
+void tank_odom_test(){
   odom_constants();
   chassis.set_coordinates(0, 0, 0);
   chassis.turn_to_point(24, 24);
-  chassis.drive_to_point(24, 24);
-  chassis.drive_to_point(0, 0);
+  chassis.drive_to_point(24,24);
+  chassis.drive_to_point(0,0);
   chassis.turn_to_angle(0);
 }
 
@@ -128,8 +119,7 @@ void tank_odom_test()
  * end where it started.
  */
 
-void holonomic_odom_test()
-{
+void holonomic_odom_test(){
   odom_constants();
   chassis.set_coordinates(0, 0, 0);
   chassis.holonomic_drive_to_pose(0, 18, 90);
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -5,17 +5,17 @@ using signature = vision::signature;
 using code = vision::code;
 
 // A global instance of brain used for printing to the V5 Brain screen.
-brain Brain;
+brain  Brain;
 digital_out matchloader = digital_out(Brain.ThreeWirePort.G);
 digital_out outputblocker = digital_out(Brain.ThreeWirePort.F);
 digital_out descorer = digital_out(Brain.ThreeWirePort.A);
 
-// The motor constructor takes motors as (port, ratio, reversed), so for example
-// motor LeftFront = motor(PORT1, ratio6_1, false);
+//The motor constructor takes motors as (port, ratio, reversed), so for example
+//motor LeftFront = motor(PORT1, ratio6_1, false);
 
-// Add your devices below, and don't forget to do the same in robot-config.h:
+//Add your devices below, and don't forget to do the same in robot-config.h:
 
-void vexcodeInit(void)
-{
+
+void vexcodeInit( void ) {
   // nothing to initialize
 }
